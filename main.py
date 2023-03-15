f46d96558de4f749af206a4452c797b23ce91740
DutchCodeCurious <114599908+DutchCodeCurious@users.noreply.github.com>
2023-03-15 16:11:07 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line
+leek_price = 2
+leeks = "leek 4"
+sum_total = int(leeks[5]) * leek_price
+print("Leek is " + str(leek_price) + " euro per kilo.")
+
+broccoli_price = 2.34
+broccoli_order = 1.6
+brocco_total = round(broccoli_price * broccoli_order, 2)
+print(str(broccoli_order) + " broccoli costs " + str(brocco_total) +"e")
